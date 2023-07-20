bfb3cfc4ea3241f4ae8377c4a001e49ecb5f7e7d
nabendu82 <nabendu.biswas@gmail.com>
2023-07-20 20:55:08 +0530

    JS 2

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -7,19 +7,72 @@
 // console.log(age);
 // console.log(isDeveloper);
 
-function varExp(){
-    if(true){
-        var num1 = 10;
-        console.log("Inside if var " + num1);
-    }
-    console.log("Outside if var " + num1);
-}
+// function varExp(){
+//     if(true){
+//         var num1 = 10;
+//         console.log("Inside if var " + num1);
+//     }
+//     console.log("Outside if var " + num1);
+// }
 
-varExp();
-console.log("Outside fuction var " + num1);
+// varExp();
+// console.log("Outside fuction var " + num1);
 
 // if(true){
 //     let num2 = 20;
 //     console.log("Inside if let " + num2);
 // }
 // console.log("Outside if let " + num2);
+
+//Datatypes
+// const firstName = 'Nabendu';
+// const age = 41;
+// const rating = 5.5;
+// const isDeveloper = true;
+// const x = null;
+// let z;
+
+// console.log(typeof firstName);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isDeveloper);
+// console.log(typeof x);
+// console.log(typeof z);
+
+// if(typeof x == 'object'){
+//     console.log('x is null');
+// }
+
+//String concatenation
+// const name = 'Nabendu';
+// const age = 41;
+
+// console.log('My name is ' + name + ' and i am ' + age + ' year\'s old.');
+// console.log(`My name is ${name} and i am ${age} year's old.`);
+
+//String Methods
+const str = 'Hello JavaScript';
+const str2 = 'React'
+const num = 200000;
+const str3 = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str3.split(' '));
+console.log(str3.split(''));
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.includes('Java'));
+console.log(str.includes('Cobol'));
+console.log(str.substring(0,7));
+console.log(str.concat(' ', str2));
+console.log(str.concat(', ', str2));
+console.log(str.endsWith('best!'));
+console.log(str.endsWith('ript'));
+console.log(str2.startsWith('Re'));
+console.log(str2.startsWith('ript'));
+console.log(num.toString().substring(0,2));
+console.log(str.slice(6));
+
+
+
+
