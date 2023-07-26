96d48223a048b4b094d15a196878f4f017841c28
nabendu82 <nabendu.biswas@gmail.com>
2023-07-26 21:31:01 +0530

    DOM done

--- DOMbasics/main.js
+++ DOMbasics/main.js
@@ -19,21 +19,47 @@
 // }
 
 //DOM manipulation
-const ul = document.querySelector('.items');
-// ul.remove();
-ul.firstElementChild.textContent = 'HTML';
-ul.children[1].innerText = 'CSS';
-ul.lastElementChild.innerHTML = '<h3>JavaScript</h3><p>Web Language</p>';
+// const ul = document.querySelector('.items');
+// // ul.remove();
+// ul.firstElementChild.textContent = 'HTML';
+// ul.children[1].innerText = 'CSS';
+// ul.lastElementChild.innerHTML = '<h3>JavaScript</h3><p>Web Language</p>';
 
 //Event Listener
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', e => {
-    e.preventDefault()
-    console.log(e.clientX)
-    console.log(e.clientY)
-    console.log(e.target.value)
-    btn.style.backgroundColor = 'blue';
-    btn.style.textTransform = 'uppercase';
-    btn.style.fontSize = '16px';
-})
+// btn.addEventListener('click', e => {
+//     e.preventDefault()
+//     console.log(e.clientX)
+//     console.log(e.clientY)
+//     console.log(e.target.value)
+//     btn.style.backgroundColor = 'blue';
+//     btn.style.textTransform = 'uppercase';
+//     btn.style.fontSize = '16px';
+// })
+
+//Form manipulation
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector(".msg");
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerText = 'Please enter all details';
+        setTimeout(() => msg.remove(), 2000);
+    } else {
+        const li = document.createElement('li');
+        li.append(`${nameInput.value} : ${emailInput.value}`);
+        userList.append(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
+
+
