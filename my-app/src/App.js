d4adf47c73faf394e2ed81bc530f3c291ce66aa3
nabendu82 <nabendu.biswas@gmail.com>
2023-08-11 21:34:36 +0530

    React 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import { Greet, ReactGreet } from './components/Greet';
+// import { Greet, ReactGreet } from './components/Greet';
 import Greeting from './components/Greeting';
 import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <ReactGreet />
-      <Greeting />
-      <Welcome />
+      {/* <Greet />
+      <ReactGreet /> */}
+      <Greeting name="JavaScript">Language of the Web</Greeting>
+      <Greeting name="Golang" />
+      <Greeting name="Swift" />
+      <hr />
+      <Welcome name="Java" type="OOP" />
+      <Welcome name="C" type="FP" />
+      <Welcome name="Flutter" type="OOP">Language for cross mobile development</Welcome>
     </div>
   );
 }
--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 
 class Welcome extends Component {
     render() {
+        const { name, type, children } = this.props;
+
         return (
-            <h1>Welcome to React</h1>
+            <>
+                <h1>Welcome {name} which is {type}</h1>
+                <p>{children}</p>
+            </>
         )
     }
 }
