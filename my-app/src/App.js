db30302c454b52d2d28d125e1c534555a79b9b2f
nabendu82 <nabendu.biswas@gmail.com>
2023-08-14 21:29:19 +0530

    Classes done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-// import { Greet, ReactGreet } from './components/Greet';
-import Greeting from './components/Greeting';
-import Welcome from './components/Welcome';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <ReactGreet /> */}
-      <Greeting name="JavaScript">Language of the Web</Greeting>
-      <Greeting name="Golang" />
-      <Greeting name="Swift" />
-      <hr />
-      <Welcome name="Java" type="OOP" />
-      <Welcome name="C" type="FP" />
-      <Welcome name="Flutter" type="OOP">Language for cross mobile development</Welcome>
+      <Counter />
     </div>
   );
 }
